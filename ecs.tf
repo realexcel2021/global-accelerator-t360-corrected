2f6986a5907ebdc96b1a671575cc3aeb052cbfbb
Sheriff Joe <89150996+realexcel2021@users.noreply.github.com>
2024-07-13 00:23:15 -0700

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -5,6 +5,7 @@
 resource "aws_ecr_repository" "foo" {
   name                 = "${random_pet.this.id}-load_testing-image"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
